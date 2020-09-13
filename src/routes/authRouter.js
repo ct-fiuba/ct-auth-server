bd4fd8bbfca9ef0469f0f8a46e2c0b2675e21cf8
Oli <olifer97@gmail.com>
2020-09-13 19:54:11 -0300

    add middleware validator.

--- src/routes/authRouter.js
+++ src/routes/authRouter.js
@@ -1,5 +1,7 @@
 const express = require('express');
 
+const bodyValidator = require('../middlewares/bodyValidatorMiddleware')();
+
 const firebaseGateway = require('../gateways/firebaseGateway');
 const authService = require('../services/authService');
 const authController = require('../controllers/authController')(authService(firebaseGateway()));
@@ -8,7 +10,7 @@ module.exports = function authRouter() {
   return express.Router().use(
     '/',
     express.Router()
-      .post('/signUp', authController.signUp)
-      .post('/signIn', authController.signIn)
+      .post('/signUp', bodyValidator.authValidations, bodyValidator.validate, authController.signUp)
+      .post('/signIn', bodyValidator.authValidations, bodyValidator.validate, authController.signIn)
   );
 };
