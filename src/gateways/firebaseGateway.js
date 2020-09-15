ff82850639e9b1461c2f361bed22d7b402e75735
Oli <olifer97@gmail.com>
2020-09-15 18:01:25 -0300

    refactor gateway.

--- src/gateways/firebaseGateway.js
+++ src/gateways/firebaseGateway.js
@@ -7,28 +7,25 @@ module.exports = function firebaseGateway() {
     prefixUrl: 'https://identitytoolkit.googleapis.com/v1'
   });
 
+  const requestAuthFirebase = (endpoint, data) => {
+    return firebaseAPI.post(`accounts:${endpoint}?key=${process.env.FIREBASE_API_KEY}`, { json: { ...data, returnSecureToken: true } })
+      .then(firebaseResponse => {
+        const { idToken, email, refreshToken, expiresIn, localId } = JSON.parse(firebaseResponse.body);
+        return { idToken, email, refreshToken, expiresIn, userId: localId };
+      })
+      .catch(error => {
+        const status = error.response.statusCode;
+        const message = JSON.parse(error.response.body).error.message;
+        throw new RequestError(message, status);
+      });
+  }
+
   const signUp = async userInfo => {
-    try {
-      let firebaseResponse = await firebaseAPI.post(`accounts:signUp?key=${process.env.FIREBASE_API_KEY}`, { json: { ...userInfo, returnSecureToken: true } });
-      const { idToken, email, refreshToken, expiresIn, localId } = JSON.parse(firebaseResponse.body);
-      return { idToken, email, refreshToken, expiresIn, userId: localId };
-    } catch (error) {
-      const status = error.response.statusCode;
-      const message = JSON.parse(error.response.body).error.message;
-      throw new RequestError(message, status);
-    }
+    return requestAuthFirebase('signUp', userInfo);
   };
 
   const signIn = async credentials => {
-    try {
-      let firebaseResponse = await firebaseAPI.post(`accounts:signInWithPassword?key=${process.env.FIREBASE_API_KEY}`, { json: { ...credentials, returnSecureToken: true } });
-      const { idToken, email, refreshToken, expiresIn, localId } = JSON.parse(firebaseResponse.body);
-      return { idToken, email, refreshToken, expiresIn, userId: localId };
-    } catch (error) {
-      const status = error.response.statusCode;
-      const message = JSON.parse(error.response.body).error.message;
-      throw new RequestError(message, status);
-    }
+    return requestAuthFirebase('signInWithPassword', credentials);
   };
 
   return {
