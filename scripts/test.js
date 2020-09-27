dd4d223895fd0120260cfd91924160a70ed0cc93
Oli <olifer97@gmail.com>
2020-09-27 15:05:29 -0300

    change script.

--- scripts/test.js
+++ scripts/test.js
@@ -7,7 +7,7 @@ const env = argv._[0];
 const env_command = env ? `./node_modules/.bin/env-cmd -f  ./.env.${env}` : '';
 
 spawn(
-  `node setup.js && ${env_command} jest int`,
+  `${env_command} jest int`,
   {
     stdio: 'inherit',
     shell: true
