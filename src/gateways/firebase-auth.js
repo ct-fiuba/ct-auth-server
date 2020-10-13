f80bcb7f27cdc200a8ab37be67b9501f61d7932a
Nacho Raik <ignacioraiksg@gmail.com>
2020-10-13 00:54:12 -0300

    Changes to support building and integrating all servers

--- src/gateways/firebase-auth.js
+++ src/gateways/firebase-auth.js
@@ -1,7 +1,9 @@
 const admin = require('firebase-admin');
 
 module.exports = function auth() {
-  if (process.env.TESTING) return {};
+  if (process.env.TESTING === 'true') {
+    return {};
+  }
 
   const creds = JSON.parse(process.env.GOOGLE_SERVICE_CREDS);
 
